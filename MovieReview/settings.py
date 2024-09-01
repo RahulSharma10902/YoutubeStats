62da4ad4e44c0436e85bd3030ef8d322554f60c6
RahulSharma10902 <158229141+RahulSharma10902@users.noreply.github.com>
2024-09-01 17:39:27 +0530

    4th commit

--- MovieReview/settings.py
+++ MovieReview/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5hnq96-gp3a_f12#c3ddg%me)l2#b$)lz947*pzv2e4g!epw3)"
+#"django-insecure-5hnq96-gp3a_f12#c3ddg%me)l2#b$)lz947*pzv2e4g!epw3)"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower()=="true"
+ALLOWED_HOSTS=os.environ.get("ALLOWED_HOSTS").split(" ")
 
 ALLOWED_HOSTS = []
 
@@ -81,7 +83,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+database_url=os.environ.get("DATABASE_URL")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
